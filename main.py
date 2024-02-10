0035438d6a8b9721ba7163fcd94da34d87017013
tkt989 <tkt989.dev@gmail.com>
2024-02-10 15:14:29 +0000

    update

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ node = t.parseToNode(text)
 words = []
 while (node):
     word_type = node.feature.split(",")[0]
-    if node.surface == "の":
-        print(node.feature)
     if word_type in["名詞"] and node.surface not in stopwords:
         words.append(node.surface)
     node = node.next
